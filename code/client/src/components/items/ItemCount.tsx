6a6662c27e1e042375ac823df5462a1ba4d03010
Benoit Simard <contact@bsimard.com>
2025-03-17 10:01:20 +0100

    [client] better count display

--- code/client/src/components/items/ItemCount.tsx
+++ code/client/src/components/items/ItemCount.tsx
@@ -16,19 +16,23 @@ export const ItemCounts: FC<ItemCountsProps> = ({ data }) => {
   return (
     <ul className="list-inline my-1">
       <li className={cx('list-inline-item pe-1', isNil(data.companiesCount) && 'text-muted')}>
-        {!isNil(data.companiesCount) ? data.companiesCount : '**'} <BsBuildings />
+        {!isNil(data.companiesCount) ? data.companiesCount : '**'}
+        <BsBuildings />
       </li>
       <li className={cx('list-inline-item pe-1', isNil(data.addressesCount) && 'text-muted')}>
-        {!isNil(data.addressesCount) ? data.addressesCount : '**'} <BsGeoAlt />
+        {!isNil(data.addressesCount) ? data.addressesCount : '**'}
+        <BsGeoAlt />
       </li>
       <li className={cx('list-inline-item pe-1', isNil(data.peopleCount) && 'text-muted')}>
-        {!isNil(data.peopleCount) ? data.peopleCount : '**'} <BsPerson />
+        {!isNil(data.peopleCount) ? data.peopleCount : '**'}
+        <BsPerson />
       </li>
       <li className={cx('list-inline-item pe-1', isNil(data.countriesCount) && 'text-muted')}>
-        {!isNil(data.countriesCount) ? data.countriesCount : '**'} <BsFlag />
+        {!isNil(data.countriesCount) ? data.countriesCount : '**'}
+        <BsFlag />
       </li>
       <li className={cx('list-inline-item pe-1', isNil(data.messagesCount) && 'text-muted')}>
-        {!isNil(data.messagesCount) ? data.messagesCount : '**'} {data.messagesCount}{' '}
+        {!isNil(data.messagesCount) ? data.messagesCount : '**'}
         <BsJournalText />
       </li>
     </ul>
