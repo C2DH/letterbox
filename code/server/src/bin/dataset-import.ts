99350a99a85be8ccd2ab73bae62d7f347a5c91cd
Paul Girard <paul@ouestware.com>
2025-05-14 09:19:27 +0200

    [server] added file pattern arg in data import script

--- code/server/src/bin/dataset-import.ts
+++ code/server/src/bin/dataset-import.ts
@@ -2,16 +2,19 @@ import { Services } from '../services';
 import { DatasetImport } from '../services/dataset/import';
 import { DatasetIndexation } from '../services/dataset/indexation';
 
-async function exec() {
+async function exec(args: string[]) {
+  const usefulArgs = args.slice(2);
+  const pdfFilePattern = usefulArgs.length > 0 ? new RegExp(usefulArgs[0]) : undefined;
+
   const datasetIndexation = Services.get(DatasetIndexation);
   const dataprep = Services.get(DatasetImport);
-  const result = await dataprep.doImport();
+  const result = await dataprep.doImport(pdfFilePattern);
   console.log('Import result', result);
   const indexation = await datasetIndexation.doIndexation();
   console.log('Indexation result', indexation);
 }
 
-exec()
+exec(process.argv)
   .then(() => process.exit(0))
   .catch((e) => {
     console.error(e);
