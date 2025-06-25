e4f6ffefa15478be6b3dc68fdec9d1aada895332
Paul Girard <paul@ouestware.com>
2025-06-25 09:51:57 +0200

    [client] extend timeline scope

--- code/client/src/config.ts
+++ code/client/src/config.ts
@@ -1,6 +1,6 @@
 export default {
   pdfURLPrefix: import.meta.env.VITE_PDF_URL || 'http://localhost/pdf/',
   // TODO: Cache some query instead of hardcoding year extents here:
-  minYear: 1961,
+  minYear: 1900,
   maxYear: 2016,
 };
